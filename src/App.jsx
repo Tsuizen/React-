3ba633fefd5cae0801542dee911e244cdfc2061d
Tsuzien <personal_rogers@outlook.com>
2021-06-29 16:05:02 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Button, message } from 'antd'
+import { Route, Switch } from 'react-router-dom'
+
+import Login from './pages/Login/index'
+import Admin from './pages/Admin/index'
 
 export default function App() {
-
-  const handleClick = () => {
-    message.success('成功了')
-  }
-
   return (
-    <div>
-      <Button type="primary" onClick={handleClick}>Primary</Button>
-    </div>
+    <>
+    {/* 只匹配一个 */}
+      <Switch> 
+        <Route path="/login" component={Login}></Route>
+        <Route path="/" component={Admin}></Route>
+      </Switch>
+    </>
   )
 }
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-/* 登陆路由组件 */
 export default function index() {
   return (
-    <div>
-      Login
-    </div>
+    <>
+      Admin
+    </>
   )
 }
